56e1e1e5a36d4aec2d9b4349601e4d6bc6d4eb8b
Admin <Admin@DESKTOP-RSJJARR>
2021-02-15 11:51:31 +0200

    fixed

--- Recursion_homework/src/homework_17_02/problem2.java
+++ Recursion_homework/src/homework_17_02/problem2.java
@@ -7,19 +7,19 @@ public class problem2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
 		int number = scan.nextInt();
-		System.out.println(digits(number,0));
+		System.out.println(digitsSum(number,0));
 		
 		scan.close();;
 	}
 	
-	public static int digits(int number, int sum) {
+	public static int digitsSum(int number, int sum) {
 		if(number/10<1) {
 			sum+=number;
 			return sum;
 		}
 		if(number/10>=1) {
 			sum +=number%10;
-			return digits(number/10,sum);
+			return digitsSum(number/10,sum);
 			
 		}
 		return 0;
